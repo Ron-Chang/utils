5a83dc2bb2957a70ca6e33d717bb20340f551d67
Ron-Chang <ron.hsien.chang@gmail.com>
2020-04-08 11:07:54 +0800

    Add args for switching diff branch default value is develop

--- gitpull.sh
+++ gitpull.sh
@@ -1,5 +1,6 @@
+BRANCH=${1:-develop}
 CURRENT_LOCATION=$PWD
-PROJECT_NAME=${1:-$(basename $PWD)}
+PROJECT_NAME=$(basename $PWD)
 PROJECT_HEAD=$(cat $CURRENT_LOCATION/.git/HEAD)
 PROJECT_BRANCH=$(echo $PROJECT_HEAD | awk -F'/' '{print $NF}')
 SUBMODULE_PATH=$(git submodule | awk 'NR==1 {print $2}')
@@ -8,21 +9,17 @@ SUBMODULE_NAME=$(echo $SUBMODULE_PATH | awk -F'/' '{print $NF}')
 SUBMODULE_HEAD=$(cat $CURRENT_LOCATION/.git/modules/$SUBMODULE_PATH/HEAD)
 SUBMODULE_BRANCH=$(echo $SUBMODULE_HEAD | awk -F'/' '{print $NF}')
 
-if [[ $PROJECT_BRANCH != develop ]]
+if [[ $PROJECT_BRANCH != $BRANCH ]]
 then
-    git checkout develop
+    git checkout $BRANCH
 fi
-echo "[PROJECT]"
-echo "[$PROJECT_NAME] | PROJECT BRANCH: [$PROJECT_BRANCH]"
-git pull
+echo "\nPROJECT: [$PROJECT_NAME] | BRANCH: [$PROJECT_BRANCH] -> [$BRANCH]"; git pull
 
 cd "$CURRENT_LOCATION/$SUBMODULE_PATH"
-if [[ $SUBMODULE_BRANCH != develop ]]
+if [[ $SUBMODULE_BRANCH != $BRANCH ]]
 then
-    git checkout develop
+    git checkout $BRANCH
 fi
-echo "[SUBMODULE]"
-echo "[$SUBMODULE_NAME] BRANCH: [$SUBMODULE_BRANCH]"
-git pull
+echo "\nSUBMODULE: [$SUBMODULE_NAME] BRANCH: [$SUBMODULE_BRANCH] -> [$BRANCH]"; git pull
 
 cd $CURRENT_LOCATION
