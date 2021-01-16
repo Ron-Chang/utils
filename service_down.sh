7c92091858cfae6a333e26519d5d0d84da640ea5
Ron <ron.hsien.chang@gmail.com>
2021-01-16 22:07:02 +0800

    update service_up and service_down

--- service_down.sh
+++ service_down.sh
@@ -1,5 +1,3 @@
-USER="`whoami`"
-CURRENT_DIR=$(pwd)
 PROJECT_PATH="`git rev-parse --show-toplevel`"
 PROJECT_NAME="`basename ${PROJECT_PATH}`"
 CONTAINER=${1:-$PROJECT_NAME}
@@ -19,13 +17,9 @@ YELLOW_ON_T='\x1b[5;33;40m'
 RED='\x1b[5;31;40m'
 ON_RED='\x1b[5;30;41m'
 
-SERVICE_NAME="`awk '{split($0,N,"_"); print N[1]"_service"}' <<< $CONTAINER`"
-DOWN="${BLUE}  ${ON_BLUE} DOWN ${BLUE_ON_YELLOW} ${RESET}"
-SERVICE="${ON_YELLOW}  SERVICE${ON_RED} ${SERVICE_NAME} ${RED} ${RESET}"
-INFO="${DOWN}${SERVICE}"
-echo ${INFO}
-
-cd "${HOME}/${SERVICE_NAME}"
-docker-compose down
-cd "${CURRENT_DIR}"
+SERVICE_NAME="`echo ${CONTAINER} | awk '{split($0,N,"_"); print N[1]"_service"}'`"
+UP="${BLUE}${ON_BLUE} DOWN ${BLUE_ON_YELLOW} ${RESET}"
+SERVICE="${ON_YELLOW} SERVICE ${YELLOW_ON_RED} ${ON_RED} ${SERVICE_NAME} ${RED} ${RESET}"
+echo "${UP}${SERVICE}"
+docker-compose -f "${HOME}/${SERVICE_NAME}/docker-compose.yml" down
 
--- service_up.sh
+++ service_up.sh
@@ -1,5 +1,3 @@
-USER="`whoami`"
-CURRENT_DIR=$(pwd)
 PROJECT_PATH="`git rev-parse --show-toplevel`"
 PROJECT_NAME="`basename ${PROJECT_PATH}`"
 CONTAINER=${1:-$PROJECT_NAME}
@@ -8,24 +6,18 @@ RESET='\x1b[0m'
 
 BLUE='\x1b[5;34;40m'
 ON_BLUE='\x1b[5;30;44m'
-BLUE_ON_YELLOW='\x1b[3;34;43m'
 
-YELLOW='\x1b[5;33;40m'
 ON_YELLOW='\x1b[5;30;43m'
-YELLOW_ON_RED='\x1b[5;33;41m'
+YELLOW_ON_BLUE='\x1b[5;33;44m'
 RED_ON_YELLOW='\x1b[5;31;43m'
-YELLOW_ON_T='\x1b[5;33;40m'
 
 RED='\x1b[5;31;40m'
 ON_RED='\x1b[5;30;41m'
 
-SERVICE_NAME="`awk '{split($0,N,"_"); print N[1]"_service"}' <<< $CONTAINER`"
-UP="${BLUE}  ${ON_BLUE} UP ${BLUE_ON_YELLOW} ${RESET}"
-SERVICE="${ON_YELLOW}  SERVICE${ON_RED} ${SERVICE_NAME} ${RED} ${RESET}"
+SERVICE_NAME="`echo ${CONTAINER} | awk '{split($0,N,"_"); print N[1]"_service"}'`"
+UP="${BLUE}  ${ON_BLUE} UP ${YELLOW_ON_BLUE}  ${RESET}"
+SERVICE="${ON_YELLOW} SERVICE ${RED_ON_YELLOW}  ${ON_RED} ${SERVICE_NAME} ${RED} ${RESET}"
 INFO="${UP}${SERVICE}"
 echo ${INFO}
-
-cd "${HOME}/${SERVICE_NAME}"
-docker-compose up -d
-cd "${CURRENT_DIR}"
+docker-compose -f "${HOME}/${SERVICE_NAME}/docker-compose.yml" up -d
 
